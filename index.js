f4e57e3eae132b2e98fd536936dc71d567df9d65
minheekangg <minheekangg@gmail.com>
2018-11-18 23:17:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching (collection, name) {
+  const result = collection.filter(word => word.toUpperCase() === name.toUpperCase());
+  return result
+}
+
+function fuzzyMatch(collection, match){
+  const wordLength = match.length
+  const result = collection.filter(word => word.substring(0, wordLength) === match);
+  // debugger;
+  return result
+}
+
+function matchName(collection, drivername) {
+  const result = collection.filter(each => each.name === drivername);
+  // debugger;
+  return result
+}
